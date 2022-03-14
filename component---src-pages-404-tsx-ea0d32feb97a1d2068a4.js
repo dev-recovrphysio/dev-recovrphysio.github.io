6cbf1ccb77331492d115331e0e310459cd1170d6
dev <dev-recovrphysio@gmail.com>
2022-03-14 22:21:47 +0100

    chore: test

---
+++ component---src-pages-404-tsx-ea0d32feb97a1d2068a4.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdev_recovrphysio=self.webpackChunkdev_recovrphysio||[]).push([[218],{938:function(e,t,n){n.r(t);var u=n(7294),l=n(3049),r=n(9470);t.default=function(){return u.createElement(l.Z,null,u.createElement(r.Z,{title:"404: Not found"}),u.createElement("h1",null,"NOT FOUND"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-ea0d32feb97a1d2068a4.js.map
---
+++ component---src-pages-page-2-tsx-e2a733b55adb90e0dcfd.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdev_recovrphysio=self.webpackChunkdev_recovrphysio||[]).push([[473],{1922:function(e,t,n){n.r(t);var l=n(7294),c=n(1597),o=n(3049),r=n(9470);t.default=function(){return l.createElement(o.Z,null,l.createElement(r.Z,{title:"Page two"}),l.createElement("h1",null,"Hi from the second page"),l.createElement("p",null,"Welcome to page 2"),l.createElement(c.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-tsx-e2a733b55adb90e0dcfd.js.map
