da995443c6a2cb56c0cc1972871d04db16f74b83
dev-recovrphysio <dev-recovrphysio@gmail.com>
2022-04-24 22:58:02 +0200

    chore: new features - logo change, service descriptions, team images, opening soon banner

---
+++ component---src-pages-404-tsx-bf7718fdd94982f23675.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdev_recovrphysio=self.webpackChunkdev_recovrphysio||[]).push([[218],{938:function(e,t,n){n.r(t);var u=n(7294),l=n(2979),r=n(9470);t.default=function(){return u.createElement(l.Z,null,u.createElement(r.Z,{title:"404: Not found"}),u.createElement("h1",null,"NOT FOUND"),u.createElement("p",null,"You just hit a route that doesn't exist"))}}}]);
